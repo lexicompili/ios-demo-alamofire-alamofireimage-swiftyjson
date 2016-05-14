75e849e79650dbf5c97e37740a120969155ddf30
Mary Alexis Solis <maryalexissolis@gmail.com>
2016-05-14 14:11:21 +0800

    Initial Commit

---
+++ DevCon iOS App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DevCon iOS App
+//
+//  Created by Mary Alexis Solis on 14/05/2016.
+//  Copyright © 2016 Mary Alexis Solis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
